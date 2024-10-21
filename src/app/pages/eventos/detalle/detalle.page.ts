78d63d79dadc5f3b335d8f2dd2a3f1080625a757
saintcarti <xa.liberona@duocuc.cl>
2024-10-21 02:59:07 -0300

    Agregue detalle , eventos lista

--- src/app/pages/eventos/detalle/detalle.page.ts
+++ src/app/pages/eventos/detalle/detalle.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallePage implements OnInit {
 
-  constructor() { }
+  evento :any;
+  constructor(private activated:ActivatedRoute,
+    private router:Router) {
+      this.activated.queryParams.subscribe(params=>{
+        this.evento = JSON.parse(params['event'])
+      })
+     }
 
   ngOnInit() {
   }
 
+  regresar(){
+    this.router.navigate(['/tabs/tab2']);
+  }
+
+  getImagePath(imagen: string): string {
+    return `assets/Imagenes/${imagen}`; // Asumiendo que tus imágenes están en assets/Imagenes/
+  }
 }
