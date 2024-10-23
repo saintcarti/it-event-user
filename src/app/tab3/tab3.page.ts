dbe17f12d84b5e6eb84583a2f8b97c48065e9918
saintcarti <xa.liberona@duocuc.cl>
2024-10-23 06:18:59 -0300

    subida

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ParticipaService } from '../services/participa.service';
 import { Router } from '@angular/router';
 import { Participacion } from 'src/interfaces/participacion';
@@ -8,15 +8,17 @@ import { Participacion } from 'src/interfaces/participacion';
   templateUrl: './tab3.page.html',
   styleUrls: ['./tab3.page.scss'],
 })
-export class Tab3Page implements OnInit {
+export class Tab3Page implements OnInit, OnDestroy {
   participacion: any[] = [];
   email: string | null = sessionStorage.getItem('email');
+  intervalId: any; // Variable para almacenar el ID del intervalo
 
   constructor(private participacionservice: ParticipaService, private router: Router) {}
 
   ngOnInit() {
     if (this.email) {
       this.loadEvents(this.email);
+      this.startAutoRefresh(); // Iniciar la actualización automática
     } else {
       console.error('No se encontró el correo electrónico en sessionStorage');
     }
@@ -33,18 +35,25 @@ export class Tab3Page implements OnInit {
     );
   }
 
+  startAutoRefresh() {
+    this.intervalId = setInterval(() => {
+      this.loadEvents(this.email!); // Recarga los eventos
+    }, 5000); // Cada 5 segundos
+  }
+
   borrarEvento(id: number) {
-    // Lógica para borrar la participación usando el servicio
     this.participacionservice.deleteParticipacion(id).subscribe(() => {
-      // Filtra la lista de participaciones para eliminar la que fue borrada
       this.participacion = this.participacion.filter(evento => evento.id !== id);
     }, (error) => {
       console.error("Error al borrar la participación", error);
     });
   }
   
-
   dejarOpinion(id: number) {
     this.router.navigate(['/dejar-opinion', { eventoId: id }]);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId); // Limpiar el intervalo al destruir el componente
   }
 }
