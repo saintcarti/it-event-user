fc9ea2cfb215645432a54ae0e14c3b219ad21d0b
juakrok <joa.osses@duocuc.cl>
2024-11-26 16:49:50 -0300

    cambio colores

--- src/app/pages/comentarios/dejar-opinion/dejar-opinion.page.ts
+++ src/app/pages/comentarios/dejar-opinion/dejar-opinion.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ParticipaService } from 'src/app/services/participa.service';
 import { ComentarEvento } from 'src/interfaces/comentarios';
-
+import { AlertController } from '@ionic/angular'; // Importa AlertController
 
 @Component({
   selector: 'app-dejar-opinion',
@@ -16,15 +16,33 @@ export class DejarOpinionPage {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private participacionservice: ParticipaService
+    private participacionservice: ParticipaService,
+    private alertController: AlertController // Inyecta AlertController
   ) {
     this.eventoId = Number(this.route.snapshot.paramMap.get('eventoId'));
   }
 
+  // Método para mostrar la alerta
+  async mostrarAlerta(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: mensaje,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  // Método para enviar el comentario
   enviarComentario() {
+    if (!this.comentario.trim()) { // Validamos si el comentario está vacío
+      this.mostrarAlerta('Debes rellenar el comentario antes de enviarlo.'); // Mostrar alerta si está vacío
+      return; // Detener la ejecución si el campo está vacío
+    }
+
     const comentarioData: ComentarEvento = {
       eventoId: this.eventoId,
-      texto: this.comentario, // Asegúrate de usar 'texto' en lugar de 'comentario'
+      texto: this.comentario, // Usamos 'texto' en lugar de 'comentario'
       email: sessionStorage.getItem('email') || ''
     };
 
@@ -38,7 +56,7 @@ export class DejarOpinionPage {
     );
   }
 
-  regresar(){
+  regresar() {
     this.router.navigate(['/tabs/tab3']);
   }
 }
